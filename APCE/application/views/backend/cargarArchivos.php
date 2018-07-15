63839b7126600768c13b3077e410287a993b423c
Bryan Amado <eamadom2@gmail.com>
2018-07-15 17:05:40 -0500

    Plantillas completas

--- APCE/application/views/backend/cargarArchivos.php
+++ APCE/application/views/backend/cargarArchivos.php
@@ -19,6 +19,17 @@
                             <option value='0'>-- Seleccione --</option>
                             <option value='1'>Provincia_exportacion_producto_pais_anio</option>
                             <option value='2'>Provincia_exportacion_anio</option>
+                            <option value='3'>Provincia_exportacion_anio_producto</option>
+                            <option value='4'>Provincia_anio</option>
+                            <option value='5'>Peru_exportacion_anio</option>
+                            <option value='6'>Departamento_exportacion_anio</option>
+                            <option value='7'>Producto_anio</option>
+                            <option value='8'>Departamento_exportacion_producto_pais_anio</option>
+                            <option value='9'>Departamento_exportacion_anio_producto</option>
+                            <option value='10'>Departamento_anio</option>
+                            <option value='11'>Pais_exportacion_anio_producto</option>
+                            <option value='12'>Partner_exportacion_anio_producto<>
+
                           </select>
                         </div>" ;?>
 
